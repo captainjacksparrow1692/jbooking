b274752bba254e91bf6e5284a3833aae1174537f
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-02-06 17:32:14 +0500

    add

---
+++ src/main/java/uzumtech/jbooking/constant/enums/RoomType.java
@@ -0,0 +1,10 @@
+package uzumtech.jbooking.constant.enums;
+
+public enum RoomType {
+    STANDARD,
+    DELUXE,
+    SUITE,
+    JUNIOR_SUITE,
+    FAMILY,
+    STUDIO
+}
--- src/main/java/uzumtech/jbooking/entity/HotelRating.java
+++ src/main/java/uzumtech/jbooking/entity/HotelRating.java
@@ -1,10 +1,9 @@
 package uzumtech.jbooking.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
 import uzumtech.jbooking.constant.enums.RatingType;
 
 import java.time.LocalDateTime;
@@ -13,6 +12,9 @@ import java.util.Map;
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "hotel_reviews")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class HotelRating {
@@ -21,15 +23,23 @@ public class HotelRating {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    Long hotelId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotel_id", nullable = false)
+    Hotel hotel;
+
+    @Column(name = "user_id", nullable = false)
     Long userId;
 
     @ElementCollection
     @CollectionTable(name = "review_ratings", joinColumns = @JoinColumn(name = "review_id"))
     @MapKeyEnumerated(EnumType.STRING)
     @Column(name = "score")
-    Map<RatingType, Integer> score;
+    Map<RatingType, Integer> scores;
 
+    @Column(length = 1000)
     String comment;
+
+    @CreationTimestamp
+    @Column(updatable = false)
     LocalDateTime createdAt;
 }
