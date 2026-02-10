fe6d032f75548d5df04dfae4cf0cda8057f9c31e
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-02-10 11:09:19 +0500

    add

---
+++ src/main/java/uzumtech/jbooking/mapper/ReviewMapper.java
@@ -0,0 +1,27 @@
+package uzumtech.jbooking.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import uzumtech.jbooking.constant.enums.RatingType;
+import uzumtech.jbooking.dto.response.ReviewsResponse;
+import uzumtech.jbooking.entity.HotelReview;
+
+import java.util.Map;
+
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
+
+    @Mapping(target = "averageScore", source = "score", qualifiedByName = "calculateAverage")
+    @Mapping(target = "userName", constant = "Guest")
+    ReviewsResponse toResponse(HotelReview hotelReview);
+
+    @Named("calculateAverage")
+    default Double calculateAverage(Map<RatingType, Integer> scores) {
+        if (scores == null || scores.isEmpty()) return 0.0;
+        return scores.values().stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+    }
+}
