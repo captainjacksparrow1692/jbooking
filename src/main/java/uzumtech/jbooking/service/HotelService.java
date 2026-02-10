62d0206d4eb61c484b6a1579afe65f830fd5f623
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-02-11 01:04:05 +0500

    add

---
+++ src/main/java/uzumtech/jbooking/service/HotelService.java
@@ -0,0 +1,20 @@
+package uzumtech.jbooking.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import uzumtech.jbooking.dto.request.HotelCreateRequest;
+import uzumtech.jbooking.dto.request.HotelSearchRequest;
+import uzumtech.jbooking.dto.response.HotelResponse;
+import uzumtech.jbooking.dto.response.HotelSearchResponse;
+
+public interface HotelService {
+
+    HotelResponse createHotel(HotelCreateRequest request);
+
+    HotelResponse getById(Long id);
+
+    Page<HotelResponse> getAll(Pageable pageable);
+
+    //поиск по дате, гостям и условиям
+    Page<HotelSearchResponse> searchHotel(HotelSearchRequest request, int page, int size);
+}
