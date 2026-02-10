d2d23916a562d9d9f14d5cf8e93d59a0bc847f7e
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-02-10 09:25:47 +0500

    add

---
+++ src/main/java/uzumtech/jbooking/dto/response/BookingCreateResponse.java
@@ -0,0 +1,24 @@
+package uzumtech.jbooking.dto.response;
+
+import uzumtech.jbooking.constant.enums.BookingStatus;
+import uzumtech.jbooking.constant.enums.PaymentType;
+import uzumtech.jbooking.dto.GuestDto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record BookingCreateResponse(
+        Long bookingId,
+        Long roomId,
+        String hotelName,
+        LocalDate checkIn,
+        LocalDate checkOut,
+        BigDecimal totalPrice,
+        BookingStatus bookingStatus,
+        PaymentType paymentType,
+        List<GuestDto> guests,
+        LocalDateTime createdAt
+) {
+}
