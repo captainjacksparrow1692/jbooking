1c874119fcfa0e60630126bd7670b13596397f9c
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-02-26 23:32:04 +0500

    add

--- src/main/java/uzumtech/jbooking/config/AppConfig.java
+++ src/main/java/uzumtech/jbooking/config/AppConfig.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 
 
 
