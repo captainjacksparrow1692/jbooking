7e0c996b44b1a8f260f6d1d594312f2f84dc672e
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2026-03-03 21:42:26 +0500

    add

--- src/main/java/uzumtech/jbooking/constant/enums/BoardBasis.java
+++ src/main/java/uzumtech/jbooking/constant/enums/BoardBasis.java
@@ -1,9 +1,9 @@
 package uzumtech.jbooking.constant.enums;
 
 public enum BoardBasis {
-    RO,  // Room Only (без питания)
-    BB,  // Bed & Breakfast (завтрак)
-    HB,  // Half Board (полупансион: завтрак + ужин)
-    FB,  // Full Board (полный пансион)
-    AI   // All Inclusive (все включено)
+    ROOM_ONLY,          // Вместо RO
+    BREAKFAST_INCLUDED, // Вместо BB
+    HALF_BOARD,         // Вместо HB
+    FULL_BOARD,         // Вместо FB
+    ALL_INCLUSIVE       // Вместо AI
 }
